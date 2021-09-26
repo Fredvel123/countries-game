249269c9dc8ed9322e83545ea8b8a5a6919cbced
Freddy <freddyvelarde59@gmail.com>
2021-09-26 18:19:37 -0400

    to back

--- src/components/LastCard.jsx
+++ src/components/LastCard.jsx
@@ -4,15 +4,21 @@ import icon from './icon.png';
 import '../styles/lastCard.css'
 
 function LastCard() {
-  const {page, count} = useContext(DataContext)
+  const {page, count, setPage, setCountries, randomCountries} = useContext(DataContext);
+  const startAgain = () => {
+    setCountries(randomCountries);
+    setPage(0)
+  }
     return (
       <Fragment>
         {page === 4 ? (
           <div className="last-card">
             <img src={icon} alt="" width="100px" />
             <div className="card-body">
+              <h3>Results</h3>
               <p>You got {count} correct answers</p>
             </div>
+            <button onClick={startAgain}>Try Again</button>
           </div>
         ) : null}
       </Fragment>
